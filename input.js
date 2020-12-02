6a016b0dba6993a8add3975f0988bbd304265434
Saad Kureshi <saq619@gmail.com>
2020-12-02 20:51:58 +0000

    Added

--- input.js
+++ input.js
@@ -1,8 +1,10 @@
+let connection;
 /**
  * Setup User Interface 
  * Specifically, so that we can handle user input via stdin
  */
-const setupInput = function() {
+const setupInput = function(conn) {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding('utf8');
@@ -15,6 +17,14 @@ const handleUserInput = (key) => {
   // \u0003 maps to ctrl+c input
   if (key === '\u0003') {
     process.exit();
+  } else if (key === "w") {
+    connection.write("Move: up");
+  } else if (key === "d"){
+    connection.write("Move: right");
+  } else if (key === "s") {
+    connection.write("Move: down");
+  } else if (key === "a") {
+    connection.write("Move: left");
   }
 }
 
--- play.js
+++ play.js
@@ -3,6 +3,6 @@ const { connect } = require('./client.js');
 const { setupInput } = require('./input.js');
 
 console.log('Connecting ...');
-connect();
+// connect();
 
-setupInput();
+setupInput(connect());
