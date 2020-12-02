7a8c15c2e2d2a65c3f7062d9839e1bdc6cbafb6f
Saad Kureshi <saq619@gmail.com>
2020-12-02 20:23:43 +0000

    Updated

---
+++ input.js
@@ -0,0 +1,24 @@
+/**
+ * Setup User Interface 
+ * Specifically, so that we can handle user input via stdin
+ */
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+  stdin.on('data', handleUserInput)
+  return stdin;
+}
+
+const handleUserInput = (key) => {
+  // \u0003 maps to ctrl+c input
+  if (key === '\u0003') {
+    process.exit();
+  }
+}
+
+module.exports = {
+  setupInput,
+  handleUserInput
+}
--- play.js
+++ play.js
@@ -1,25 +1,8 @@
 const net = require('net');
 const { connect } = require('./client.js');
+const { setupInput } = require('./input.js');
+
 console.log('Connecting ...');
 connect();
-/**
- * Setup User Interface 
- * Specifically, so that we can handle user input via stdin
- */
-const setupInput = function() {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding('utf8');
-  stdin.resume();
-  stdin.on('data', handleUserInput)
-  return stdin;
-}
-
-const handleUserInput = (key) => {
-  // \u0003 maps to ctrl+c input
-  if (key === '\u0003') {
-    process.exit();
-  }
-}
 
 setupInput();
